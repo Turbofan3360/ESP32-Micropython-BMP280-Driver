73e75b581cdeb9ae036a07da5264a6f7a6fa635d
Samuel Pontefract <zia32kvs@duck.com>
2025-06-27 17:27:53 +0200

    Made a start on reading pressure/temp values

--- bmp280.py
+++ bmp280.py
@@ -27,27 +27,38 @@ class bmp280:
         # Getting factory-programmed trim values from the BMP280's NVM
         self.get_trim_values()
     
-    def setup(self):
+    def _setup(self):
         # Waking sensor up and setting oversampling rates for pressure/temperature measurement
         self.bmp280.writeto_mem(self.bmpaddress, self.registers["measurement_ctrl"], bytes([0x57]))
         
         # Setting standby time length, IIR filter
         self.bmp280.writeto_mem(self.bmpaddress, self.registers["config"], bytes([0x10]))
     
-    def get_trim_values(self):
+    def _get_trim_values(self):
         # Reading trim values from chip
         digits = self.bmp280.readfrom_mem(self.bmpaddress, 0x88, 24)
         
-        self.trim_values["dig_t1"] = struct.unpack("<H", digits[0:2])
-        self.trim_values["dig_t2"] = struct.unpack("<h", digits[2:4])
-        self.trim_values["dig_t3"] = struct.unpack("<h", digits[4:6])
+        self.trim_values["dig_t1"] = struct.unpack("<H", digits[0:2])[0]
+        self.trim_values["dig_t2"] = struct.unpack("<h", digits[2:4])[0]
+        self.trim_values["dig_t3"] = struct.unpack("<h", digits[4:6])[0]
         
-        self.trim_values["dig_p1"] = struct.unpack("<H", digits[6:8])
-        self.trim_values["dig_p2"] = struct.unpack("<h", digits[8:10])
-        self.trim_values["dig_p3"] = struct.unpack("<h", digits[10:12])
-        self.trim_values["dig_p4"] = struct.unpack("<h", digits[12:14])
-        self.trim_values["dig_p5"] = struct.unpack("<h", digits[14:16])
-        self.trim_values["dig_p6"] = struct.unpack("<h", digits[16:18])
-        self.trim_values["dig_p7"] = struct.unpack("<h", digits[18:20])
-        self.trim_values["dig_p8"] = struct.unpack("<h", digits[20:22])
-        self.trim_values["dig_p9"] = struct.unpack("<h", digits[22:24])
+        self.trim_values["dig_p1"] = struct.unpack("<H", digits[6:8])[0]
+        self.trim_values["dig_p2"] = struct.unpack("<h", digits[8:10])[0]
+        self.trim_values["dig_p3"] = struct.unpack("<h", digits[10:12])[0]
+        self.trim_values["dig_p4"] = struct.unpack("<h", digits[12:14])[0]
+        self.trim_values["dig_p5"] = struct.unpack("<h", digits[14:16])[0]
+        self.trim_values["dig_p6"] = struct.unpack("<h", digits[16:18])[0]
+        self.trim_values["dig_p7"] = struct.unpack("<h", digits[18:20])[0]
+        self.trim_values["dig_p8"] = struct.unpack("<h", digits[20:22])[0]
+        self.trim_values["dig_p9"] = struct.unpack("<h", digits[22:24])[0]
+    
+    def _get_pressure_temp_raw(self):
+        # Burst-reading the raw temp/pressure bytes
+        pressure = self.bmp280.readfrom_mem(self.bmpaddress, self.registers["pressure"], 3)
+        temp = self.bmp280.readfrom_mem(self.bmpaddress, self.registers["temp"], 3)
+        
+        # Decoding temp/pressure bytearrays - the data comes packaged in format MSB - LSB - XLSB
+        pressure_decoded = (pressure[0] << 12) | (pressure[1] << 4) | (pressure[2] >> 4)
+        temp_decoded = (temp[0] << 12) | (temp[1] << 4) | (temp[2] >> 4)
+        
+        return pressure_decoded, temp_decoded
